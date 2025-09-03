14295a2fb532a23d69b85d9e745e77e1f7bb6519
Caramel4764 <nina.dong4764@gmail.com>
2025-09-03 11:05:52 -0700

    feat: create book report based on file directory path given. word count and total letter count included.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from stats import get_book_text
 from stats import word_count
+from stats import count_character_instance
+from stats import sorted_list
+from stats import printPrettyCharacterReport
+import sys
 
 def main():
   #print(get_book_text("./books/frankenstein.txt"))
-  book_content = get_book_text("./books/frankenstein.txt")
-  print(f"{word_count(book_content)} words found in the document")
+  if len(sys.argv)<=1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  
+  book_content = get_book_text(sys.argv[1])
+  letter_instance_dictionary = count_character_instance(book_content)
+  print("============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------")
+  print(f"Found {word_count(book_content)} total words")
+
+  #print(letter_instance_dictionary)
+  nice_list = (sorted_list(letter_instance_dictionary))
+  printPrettyCharacterReport(nice_list)
+  print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -4,5 +4,31 @@ def get_book_text(file_path):
       # f is a file object
     file_contents = f.read()
     return file_contents
+def count_character_instance(text):
+  text = text.lower()
+  instance_counter = {}
+  #check if character exists, if not add dictionary, otherwise counter 1
+  for i in range(0, len(text)):
+    curr_letter = text[i:i+1]
+    if curr_letter not in instance_counter and curr_letter!=" ":
+      instance_counter[curr_letter]=1
+    else:
+      for letter in instance_counter:
+        if (curr_letter == letter):
+          instance_counter[letter]+=1
+  return instance_counter
 def word_count(text):
   return len(text.split())
+def sorted_list(instance_dictionary):
+  sorted_list = []
+  for letter in instance_dictionary:
+    sorted_list.append({"char":letter, "num": instance_dictionary[letter]})
+  sorted_list.sort(key=return_num_key, reverse=True)
+  return sorted_list
+def return_num_key(char):
+  return char["num"]
+def printPrettyCharacterReport(sorted_list):
+  print("--------- Character Count -------")
+  for char in sorted_list:
+    if (char["char"].isalpha()):
+      print(f"{char['char']}: {char['num']}")
